9531ff0d365954354ac146e312ccc96ec1b1a444
Niels Marsman <niels.marsman@gmail.com>
2026-04-01 12:29:54 +0200

    EXP1 - Initial project setup

---
+++ settings.gradle.kts
@@ -0,0 +1,23 @@
+rootProject.name = "kotlin-expect"
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    @Suppress("UnstableApiUsage")
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+include(
+    ":core",
+)
