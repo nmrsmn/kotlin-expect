78ea8b6c014774f8f1a5099dca02982deebd7668
Niels Marsman <niels.marsman@gmail.com>
2026-04-02 13:17:59 +0200

    EXP12 - Implementation of the 'is a'-assertion, that checks if the subject is an instance of the given type

--- core/src/commonMain/kotlin/dev/nmarsman/expect/assertions/Any.kt
+++ core/src/commonMain/kotlin/dev/nmarsman/expect/assertions/Any.kt
@@ -3,6 +3,16 @@ package dev.nmarsman.expect.assertions
 import dev.nmarsman.expect.api.Assertion
 
 @Suppress("UNCHECKED_CAST")
+inline fun <reified T> Assertion.Builder<*>.isA(): Assertion.Builder<T> =
+    assert(description = "is an instance of ${T::class}") {
+        when (it) {
+            null -> fail()
+            is T -> pass()
+            else -> fail()
+        }
+    } as Assertion.Builder<T>
+
+@Suppress("UNCHECKED_CAST")
 fun <T> Assertion.Builder<T?>.isNull(): Assertion.Builder<Nothing> =
     assert(description = "is null") {
         when (it) {
--- core/src/commonTest/kotlin/dev/nmarsman/expect/assertions/AnyTest.kt
+++ core/src/commonTest/kotlin/dev/nmarsman/expect/assertions/AnyTest.kt
@@ -5,20 +5,58 @@ import dev.nmarsman.expect.api.expectThat
 import kotlin.test.DefaultAsserter.fail
 
 val AnyAssertionTest by testSuite {
-    test("isNull should pass if the subject is null") {
-        expectThat(null)
-            .isNull()
+    testSuite(name = "subject 'is A' assertions") {
+        test(name = "isA should pass if the subject is an instance of the expected type") {
+            expectThat("value")
+                .isA<String>()
+        }
+
+        test(name = "isA should pass if the subject is an instance of a subtype of the expected type") {
+            expectThat(1L)
+                .isA<Number>()
+        }
+
+        test(name = "isA should fail if the subject is not an instance of the expected type") {
+            try {
+                expectThat("value")
+                    .isA<Number>()
+
+                // Doesn't work properly until library implements AssertionFailedError from opentest4j
+                fail("Test should have thrown")
+            } catch (_: AssertionError) {
+                // Test should have thrown
+            }
+        }
+
+        test(name = "isA should fail if the subject is null") {
+            try {
+                expectThat(null)
+                    .isA<String>()
+
+                // Doesn't work properly until library implements AssertionFailedError from opentest4j
+                fail("Test should have thrown")
+            } catch (_: AssertionError) {
+                // Test should have thrown
+            }
+        }
     }
 
-    test("isNull should fail if the subject is not null") {
-        try {
-            expectThat("value" as String?)
+    testSuite(name = "subject 'is null' assertions") {
+        test("isNull should pass if the subject is null") {
+            expectThat(null)
                 .isNull()
+        }
 
-            // Doesn't work properly until library implements AssertionFailedError from opentest4j
-            fail("Test should have thrown")
-        } catch (_: AssertionError) {
-            // Test should have thrown
+        test("isNull should fail if the subject is not null") {
+            try {
+                expectThat("value" as String?)
+                    .isNull()
+
+                // Doesn't work properly until library implements AssertionFailedError from opentest4j
+                fail("Test should have thrown")
+            } catch (_: AssertionError) {
+                // Test should have thrown
+            }
         }
     }
 }
