1eeec85124297aec9e4d06cad6cbbbe19f1db678
Niels Marsman <niels.marsman@gmail.com>
2026-04-02 12:49:04 +0200

    EXP12 - Fixed detekt issue about line length and outdated documentation (generic needs to be added as @param too)

--- core/src/commonMain/kotlin/dev/nmarsman/expect/api/Assertion.kt
+++ core/src/commonMain/kotlin/dev/nmarsman/expect/api/Assertion.kt
@@ -10,16 +10,16 @@ interface Assertion {
     /**
      * Mark the result of the assertion as failed.
      *
-     * @param description An optional description of the failure, which can provide additional context about why the assertion failed.
-     * @param cause An optional Throwable that caused the failure, which can be used to provide more detailed information about the failure,
-     *              such as an exception that was thrown during the assertion.
+     * @param description An optional description of the failure. Can provide context about why the assertion failed.
+     * @param cause An optional Throwable that caused the failure. Can be used to provide more detailed information
+     *      about the failure, such as an exception that was thrown during the assertion.
      */
     fun fail(description: String? = null, cause: Throwable? = null)
 
     /**
      * Mark the result of the assertion as successful.
      *
-     * @param description An optional description of the success, which can provide additional context about why the assertion passed.
+     * @param description An optional description of the success. Can provide context about why the assertion passed.
      */
     fun pass(description: String? = null)
 
@@ -32,13 +32,13 @@ interface Assertion {
         val subject: T
 
         /**
-         * Defines an assertion on the subject of the expectation.
-         * The assertion is defined as a lambda function that takes an instance of [Assertion] and the subject of type [T] as parameters.
+         * Defines an assertion on the subject of the expectation. The assertion is defined as a lambda function that
+         * takes an instance of [Assertion] and the subject of type [T] as parameters.
          *
-         * @param description A description of the assertion, which can provide additional context about what is being asserted.
-         * @param assert The lambda function that defines the assertion logic. It receives an instance of [Assertion] and the
-         *               subject of type [T], allowing you to perform assertions on the subject and mark the assertion as
-         *               passed or failed using the [Assertion] instance.
+         * @param description A description of the assertion. Can provide context about what is being asserted.
+         * @param assert The lambda function that defines the assertion logic. It receives an instance of
+         *      [Assertion] and the subject of type [T], allowing you to perform assertions on the
+         *      subject and mark the assertion as passed or failed using the [Assertion] instance.
          */
         fun assert(
             description: String,
--- core/src/commonMain/kotlin/dev/nmarsman/expect/api/Expect.kt
+++ core/src/commonMain/kotlin/dev/nmarsman/expect/api/Expect.kt
@@ -6,7 +6,8 @@ import dev.nmarsman.expect.internal.AssertionBuilder
  * Starts an assertion on the given [subject] and returns a builder for chaining further assertions.
  * This is the entry-point for the assertion API, allowing you to create assertions on any value.
  *
- * @param subject The value to be asserted on.
+ * @param T The type of the subject being asserted on.
+ * @param subject The value ([T]) to be asserted on.
  * @return An assertion on the value of [subject].
  */
 fun <T> expectThat(subject: T): Assertion.Builder<T> =
