15570d59117bff5c5ec8b09f1ebd5f86767ab043
Niels Marsman <niels.marsman@gmail.com>
2026-04-22 09:15:55 +0200

    EXP47 - Introduced plugin to publish library

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.kotlin.multiplatform) apply true
+    alias(libs.plugins.maven.publish) apply true
     alias(libs.plugins.nmarsman.detekt) apply true
     alias(libs.plugins.test.balloon) apply true
 }
@@ -25,6 +26,45 @@ kotlin {
         commonTest.dependencies {
             implementation(libs.kotlin.test)
             implementation(libs.test.balloon.core)
+        }
+    }
+}
+
+mavenPublishing {
+    publishToMavenCentral()
+    signAllPublications()
+
+    coordinates(
+        groupId = "dev.nmarsman.expect",
+        artifactId = "kotlin-expect-core",
+        version = libs.versions.kotlin.expect.get(),
+    )
+
+    pom {
+        name.set("Kotlin Expect")
+        description.set("A Kotlin library for writing fluent and descriptive assertions, with a focus test readability.")
+        inceptionYear.set("2026")
+
+        developers {
+            developer {
+                id.set("nmrsmn")
+                name.set("Niels Marsman")
+                url.set("https://github.com/nmrsmn")
+                timezone.set("Europe/Amsterdam")
+            }
+        }
+
+        scm {
+            url.set("https://github.com/nmrsmn/kotlin-expect")
+            connection.set("git@github.com:nmrsmn/kotlin-expect.git")
+        }
+
+        licenses {
+            license {
+                name = "The MIT License (MIT)"
+                url = "https://mit-license.org/"
+                distribution = "https://mit-license.org/"
+            }
         }
     }
 }
